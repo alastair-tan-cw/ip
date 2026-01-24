de865d48e31764b2e4dd47b153c03d586685d299
Alastair Tan <atcw@hotmail.sg>
2026-01-24 19:40:37 +0800

    Rename main class and add greeting

---
+++ src/main/java/Peter.java
@@ -0,0 +1,13 @@
+public class Peter {
+    public static final String name = "Peter";
+
+    public static void main(String[] args) {
+        String menu = "____________________________________________________________\n" +
+                " Hello! I'm " + name + "\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(menu);
+    }
+}
