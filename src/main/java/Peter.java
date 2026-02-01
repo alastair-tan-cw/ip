6ee414a5de8f4a2bd36474d5d680a09c8f744738
Alastair Tan <atcw@hotmail.sg>
2026-02-01 15:23:50 +0800

    Add support for echo

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Peter {
     public static final String name = "Peter";
 
@@ -5,9 +7,22 @@ public class Peter {
         String menu = "____________________________________________________________\n" +
                 " Hello! I'm " + name + "\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+                "____________________________________________________________\n";
         System.out.println(menu);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        "Goodbye. Chat again soon?" +
+                        "\n____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" +
+                        ">> " + userInput +
+                        "\n____________________________________________________________\n");
+            }
+        }
     }
 }
