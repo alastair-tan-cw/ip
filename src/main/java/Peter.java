a09545d5394f2aab0952068e1ee5e9a7195347fa
Alastair Tan <atcw@hotmail.sg>
2026-02-01 16:55:36 +0800

    Add support for list

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Peter {
     public static final String name = "Peter";
 
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
         String menu = "____________________________________________________________\n" +
                 " Hello! I'm " + name + "\n" +
                 " What can I do for you?\n" +
@@ -18,9 +21,23 @@ public class Peter {
                         "Goodbye. Chat again soon?" +
                         "\n____________________________________________________________\n");
                 break;
+            } else if (userInput.equals("list")) {
+                if (list.isEmpty()) {
+                    System.out.println("____________________________________________________________\n" +
+                            "Nothing in list yet." +
+                            "\n____________________________________________________________\n");
+                    continue;
+                }
+
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(i + 1 + "." + list.get(i));
+                }
+                System.out.println("____________________________________________________________\n");
             } else {
+                list.add(userInput);
                 System.out.println("____________________________________________________________\n" +
-                        ">> " + userInput +
+                        ">> Added: " + userInput +
                         "\n____________________________________________________________\n");
             }
         }
